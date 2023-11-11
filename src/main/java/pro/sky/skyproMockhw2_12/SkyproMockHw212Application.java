e598f87e2e27317ffe43d344fc99272299bcd367
alex <alexsemen666653@gmail.com>
2023-11-11 16:47:19 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproMockhw2_12/SkyproMockHw212Application.java
@@ -0,0 +1,14 @@
+package pro.sky.skyproMockhw2_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproMockHw212Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkyproMockHw212Application.class, args);
+	}
+
+}
